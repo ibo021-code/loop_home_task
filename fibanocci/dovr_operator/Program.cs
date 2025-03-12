bfc935cedd320c5089f367a50b2a9ccaaf2d244e
ibrahim <ibrahimem-bb209@code.edu.az>
2025-03-12 22:44:32 +0400

    loop daxil edildi

---
+++ fibanocci/dovr_operator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+        Console.Write("daxil et ededi : ");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, c = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"{a} + {b} = {c}");
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    
+
+
---
+++ sade_murekkeb/loops_task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+using System;
+Console.WriteLine("Eded daxil edin:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 1)
+{
+    Console.WriteLine("eded ne sadedir ne murekkkeb");
+}
+else
+{
+    bool sadeEdeddir = true;
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            sadeEdeddir = false;
+        }
+    
+        
+    }
+    if(sadeEdeddir)
+    {
+        Console.WriteLine("eded sade ededdir");
+    }
+    else
+    {
+        Console.WriteLine("eded murekkeb ededdir");
+    }
+}
+
+    
